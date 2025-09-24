950793ab322516377308404e31ca986124e6bf4c
Mugilvannan <mugilvannanpichai02@gmail.com>
2025-09-24 14:41:11 +0530

    change origins

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(cors({
   origin: [
     'https://fiit-jobs-admin.vercel.app',
-    'https://fiitjobs.vercel.app'
+    'https://fiitjobs.vercel.app',
+    'http://localhost:5173'
   ]
 }));
 app.use(express.json()); 
